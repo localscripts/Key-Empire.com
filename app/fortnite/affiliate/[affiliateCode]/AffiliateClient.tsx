f2bde63460e5fe372ab4996f739521c115f5af4e
100usd <82117240+100uzd@users.noreply.github.com>
2025-08-05 21:39:44 +0200

    Update AffiliateClient.tsx

--- app/fortnite/affiliate/[affiliateCode]/AffiliateClient.tsx
+++ app/fortnite/affiliate/[affiliateCode]/AffiliateClient.tsx
@@ -16,7 +16,7 @@ export default function AffiliateClient({ affiliateCode }: { affiliateCode: stri
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white dark:bg-white dark:text-gray-900">
-      <p>Redirecting to Counter-Strike 2 page...</p>
+      <p>Redirecting to Fortnite page...</p>
     </div>
   )
 }
