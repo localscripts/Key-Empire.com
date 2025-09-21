a963a716e1af3d021ef16d8df368db177b2a9134
100usd <82117240+100uzd@users.noreply.github.com>
2025-09-21 15:08:44 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,9 +101,9 @@ export default function RootLayout({
 
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 
         <style>{`
 html {
