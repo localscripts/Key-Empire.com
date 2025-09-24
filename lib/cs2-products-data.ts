089aa4318ba47bf3c4e147b407134b2158d09a8b
100usd <82117240+100uzd@users.noreply.github.com>
2025-09-24 16:26:32 +0200

    Update cs2-products-data.ts

--- lib/cs2-products-data.ts
+++ lib/cs2-products-data.ts
@@ -9,40 +9,16 @@ export interface CS2Product {
 
 export const CS2_PRODUCTS_LIST: CS2Product[] = [
   {
-    title: "CS2 Aimbot",
+    title: "KERNAIM",
     price: "$9.99",
     resellers: "15+",
-    image: "/images/cs2-aimbot.png",
+    image: "/images/unknown.png",
   },
   {
-    title: "CS2 Wallhack",
+    title: "predator",
     price: "$7.99",
     resellers: "12+",
-    image: "/images/cs2-wallhack.png",
-  },
-  {
-    title: "CS2 ESP",
-    price: "$5.99",
-    resellers: "18+",
-    image: "/images/cs2-esp.png",
-  },
-  {
-    title: "CS2 Triggerbot",
-    price: "$4.99",
-    resellers: "10+",
-    image: "/images/cs2-triggerbot.png",
-  },
-  {
-    title: "CS2 Bhop",
-    price: "$3.99",
-    resellers: "8+",
-    image: "/images/cs2-bhop.png",
-  },
-  {
-    title: "CS2 Radar",
-    price: "$6.99",
-    resellers: "14+",
-    image: "/images/cs2-radar.png",
+    image: "/images/unknown.png",
   },
 ]
 
