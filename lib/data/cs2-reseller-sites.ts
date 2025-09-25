0b98d9dfec353cdc30c72c433aa007151d9380d6
100usd <82117240+100uzd@users.noreply.github.com>
2025-09-25 15:41:44 +0200

    Update cs2-reseller-sites.ts

--- lib/data/cs2-reseller-sites.ts
+++ lib/data/cs2-reseller-sites.ts
@@ -1,6 +1,10 @@
 import type { ResellerSite } from "../types/affiliate"
 
 export const CS2_RESELLER_SITES: ResellerSite[] = [
+  {
+    url: "https://raw.githubusercontent.com/100uzd/keyempirestuff/refs/heads/main/eternalproducts/cs2.json",
+    verified: false,
+  },
 
 ]
 
