86a66e08c529be9c77b682734c4bb6d530390672
100usd <82117240+100uzd@users.noreply.github.com>
2025-09-25 16:25:00 +0200

    Update cs2-reseller-sites.ts

--- lib/data/cs2-reseller-sites.ts
+++ lib/data/cs2-reseller-sites.ts
@@ -7,26 +7,3 @@ export const CS2_RESELLER_SITES: ResellerSite[] = [
   },
 
 ]
-
-export const KNOWN_CS2_CHEATS = [
-  "aimbot",
-  "wallhack",
-  "esp",
-  "triggerbot",
-  "bhop",
-  "radar",
-  "spinbot",
-  "norecoil",
-  "autofire",
-  "skinchanger",
-]
-
-export const CS2_PAYMENT_PATTERNS = {
-  crypto: ["bitcoin", "btc", "cryptocurrency", "crypto", "ethereum", "eth", "litecoin", "ltc", "usdt", "usdc"],
-  paypal: ["paypal", "pp", "paypal.me"],
-  stripe: ["stripe", "card", "credit card", "visa", "mastercard"],
-  cashapp: ["cashapp", "cash app", "$cashtag"],
-  venmo: ["venmo", "@venmo"],
-  steam: ["steam wallet", "steam gift", "steam card"],
-  "cs2-skins": ["cs2 skins", "counter-strike skins", "csgo skins"],
-}
