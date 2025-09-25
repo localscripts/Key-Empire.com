7bdf5ea36a4f12774fa7a819f15b0ed8e761abe0
100usd <82117240+100uzd@users.noreply.github.com>
2025-09-25 15:42:05 +0200

    Update rust-reseller-sites.ts

--- lib/data/rust-reseller-sites.ts
+++ lib/data/rust-reseller-sites.ts
@@ -1,6 +1,10 @@
 import type { ResellerSite } from "../types/affiliate"
 
 export const RUST_RESELLER_SITES: ResellerSite[] = [
+  {
+    url: "https://raw.githubusercontent.com/100uzd/keyempirestuff/refs/heads/main/eternalproducts/rust.json",
+    verified: false,
+  },
 
 ]
 
